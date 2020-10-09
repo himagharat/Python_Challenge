427edfd0d9e8184aa42a2cf5f8736770322dca40
Hima Gharat <gharat.hima2@gmail.com>
2020-10-09 13:07:22 -0400

    My First Commit

---
+++ PyBank/Main.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 12:55:14 2020
+
+@author: ghara
+"""
+
---
+++ PyPoll/Main_1.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Oct  9 13:00:17 2020
+
+@author: ghara
+"""
+
